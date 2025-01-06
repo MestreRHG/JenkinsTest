bb324b42bf21c9127eefd1456e181c17010c7fc3
João Martins <joao.p.f.martins2005@gmail.com>
2025-01-06 14:32:49 +0000

    Add project files.

---
+++ JenkinsTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
